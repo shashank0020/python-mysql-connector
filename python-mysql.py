3424e09f27d922c91e021255069f97c9eda5e2b5
shashank0020 <shashank_verma0020@outlook.com>
2014-07-03 15:48:47 +0530

    Create python-mysql.py

---
+++ python-mysql.py
@@ -0,0 +1,29 @@
+import MySQLdb
+db1 = MySQLdb.connect(host="localhost",user="root",passwd="root")
+cursor = db1.cursor()
+create_db='''CREATE DATABASE erp;'''
+cursor.execute(create_db)
+
+create_table_sql='''CREATE TABLE erp.account_inv (
+  inv_no INT NOT NULL AUTO_INCREMENT,
+  product VARCHAR(45) NOT NULL,
+  amount VARCHAR(45) NOT NULL,
+  state VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`inv_no`));'''
+
+cursor.execute(create_table_sql)
+
+insert_sql='''3.INSERT INTO erp.account_inv (inv_no, product, amount) VALUES ('SAJ001', 'PEPSI', '12');'''
+cursor.execute(insert_sql)
+
+select_sql='''SELECT * FROM erp.account_inv'''
+
+cursor.execute(select_sql)
+
+#fetch values from mysql
+
+for row in cursor.fetchall() :
+    print row
+
+db.commit()
+db.close()
